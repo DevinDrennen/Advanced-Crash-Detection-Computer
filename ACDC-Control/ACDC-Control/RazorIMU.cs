b7d616f14503dd4fb5adf0a0f5ab4f0e45ea8e9a
Omar Z <xohmz@outlook.com>
2015-09-29 21:09:20 -0400

    Comments and README
    Updated code comments and added readme file.

--- ACDC-Control/ACDC-Control/RazorIMU.cs
+++ ACDC-Control/ACDC-Control/RazorIMU.cs
@@ -7,7 +7,7 @@ using System.Threading;
 namespace ACDC_Control
 {
     /// <summary>
-    /// 
+    /// Razor IMU instance
     /// </summary>
     public class RazorIMU
     {
@@ -18,8 +18,15 @@ namespace ACDC_Control
         const int BYTES_PER_FLOAT = 4, FLOATS_PER_TRANS = 9, CHUNK_SIZE = BYTES_PER_FLOAT * FLOATS_PER_TRANS;
         private float[] data;
 
+        /// <summary>
+        /// Indicates if communication has been initilized between the sensor and netduino.
+        /// </summary>
         public bool Initilized { get; private set; }
 
+        /// <summary>
+        /// Current data chunk from sensor.
+        /// [accel x, accel y, accel z, magno x, magno y, magno z, gyro x, gyro y, gyro z]
+        /// </summary>
         public float[] Data
         {
             get { return data; }
@@ -27,10 +34,10 @@ namespace ACDC_Control
         }
 
         /// <summary>
-        /// 
+        /// Razor IMU object constructor. 
         /// </summary>
-        /// <param name="port"></param>
-        /// <param name="baudRate"></param>
+        /// <param name="port">Serial port on which this sensor resides</param>
+        /// <param name="baudRate">Baud rate sensor is set up to use. Default set.</param>
         public RazorIMU(string port, int baudRate = 57600)
         {
             // Define SYNC related byte arrays, create port, and data arrays
@@ -43,7 +50,7 @@ namespace ACDC_Control
         }
 
         /// <summary>
-        ///  Reset the binary data stream to the start
+        /// Reset the binary data stream to the start.
         /// </summary>
         /// <returns>True if initiated correctly, false otherwise.</returns>
         public bool InitializeDataStream()
@@ -52,56 +59,74 @@ namespace ACDC_Control
             int bytesRead = 0;
             Initilized = false;
 
-            if(!razorIMU.IsOpen)
-                razorIMU.Open();                                    // Open port for communication
-            razorIMU.Write(syncByteArray, 0, syncByteArray.Length); // Send sync signal
+            // If the stream reader is alive, stop it!
+            if (serialReadThread != null && serialReadThread.IsAlive)
+                serialReadThread.Abort();
 
+            // If port isn't open yet, open it.
+            if(!razorIMU.IsOpen)
+                razorIMU.Open();
+            // Send sync signal
+            razorIMU.Write(syncByteArray, 0, syncByteArray.Length); 
+
+            // Wait for # part of the sync response signal
             while (nextByte != '#')
             {
-                if (bytesRead == 37)
+                // Lets not wait forever, 
+                // after a full frame, its time to stop and report an error.
+                if (bytesRead == 37) 
                     break;
 
+                // Read in the next byte and increase how many have been read.
                 nextByte = razorIMU.ReadByte();
                 bytesRead++;
             }
 
+            // Validate sync signal a byte at a time, if fails report problem.
             for (int i = 1; i < syncOKByteArray.Length; i++)
             {
                 nextByte = razorIMU.ReadByte();
                 if (nextByte != syncOKByteArray[i])
                 {
+                    // Report problem, close port, and return a fail
                     Debug.Print("Problem connecting to Razor IMU.");
                     razorIMU.Close();
                     return false;
                 }
             }
 
+            // On success initialize stream reader and set state to initialized
             serialReadThread = new Thread(new ThreadStart(readStream));
             serialReadThread.Start();
-
             Initilized = true;
+
             return true;
         }
 
         /// <summary>
-        /// 
+        /// As long as the port is open, read in data.
         /// </summary>
         private void readStream()
         {
+            // Variables to keep track of and store data
             int bytesRead = 0;
             byte[] streamChunk = new byte[CHUNK_SIZE];
             string dataString;
 
             while(razorIMU.IsOpen)
             {
+                // Store the next byte read into the next spot on the array and increase how many bytes have been read.
                 streamChunk[bytesRead] = (byte)razorIMU.ReadByte();
                 bytesRead++;
 
+                // If CHUNK_SIZE (36) bytes have been read
                 if(bytesRead == CHUNK_SIZE)
                 {
+                    // Reset the counter and convert the array
                     bytesRead = 0;
                     convertData(streamChunk);
 
+                    // This is debugging stuff to see what the array looks like
                     dataString = "";
                     for (int i = 0; i < FLOATS_PER_TRANS; i++)
                         dataString += ((int)data[i]).ToString("D4") + ", ";
@@ -111,7 +136,7 @@ namespace ACDC_Control
         }
 
         /// <summary>
-        /// 
+        /// Convert the byte array to the floats represented byte each 4 bytes.
         /// </summary>
         /// <param name="streamChunk"></param>
         private void convertData(byte[] streamChunk)
@@ -121,6 +146,7 @@ namespace ACDC_Control
 
             lock(data)
             {
+                // Extract 4 bytes at a time to make a float
                 for(int i = 0; i < CHUNK_SIZE; i += BYTES_PER_FLOAT)
                 {
                     floatChunk[0] = streamChunk[i];
---
+++ README.md
@@ -0,0 +1,23 @@
+Advanced Crash Detection Computer (ACDC)
+========================================
+
+As our senior project my friend and I have decided to create a microcontroller system to detect a bike crash and report the event in various ways.
+
+**Notes:**
+There is a snippet of code intended to solve issues with the SDK and VS 2015, however, it should not interfere with other versions of VS.
+
+
+**Features Planned:**
+* Detection
+  * Front crash w/o falling over
+  * Crash leading fall on side
+  * Flips etc
+* Reporting
+  * Flashing Light
+  * Txt message
+  * Maybe emmit emergency RF signal
+
+**Hardware:**
+![R GPX Analysis GUI](/Photos/hardware.png?raw=true)
+* [Netduino 3 WiFi](http://www.netduino.com/netduino3wifi/specs.htm)
+* [Razor IMU](https://www.sparkfun.com/products/10736)
