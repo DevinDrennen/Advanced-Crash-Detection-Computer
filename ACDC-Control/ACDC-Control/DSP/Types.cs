c48f9ffdae8084cfaa7a5fc9b2fb8cc58bf0c5e4
Omar Z <xohmz@outlook.com>
2015-10-01 14:58:44 -0400

    Added complex type.
    Added a struct for complex numbers to use for any DSP calculations in
    the future.

---
+++ ACDC-Control/ACDC-Control/DSP/Types.cs
@@ -0,0 +1,253 @@
+using System;
+using Microsoft.SPOT;
+
+namespace ACDC_Control.DSP.Types
+{
+    public struct Complex
+    {
+        #region Special Complex
+
+        public static readonly Complex Unit = new Complex(1, 1);
+
+        public static readonly Complex One = new Complex(1);
+
+        public static readonly Complex Zero = new Complex();
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the real component.
+        /// </summary>
+        public float Real
+        { get; private set; }
+
+        /// <summary>
+        /// Gets the imaginary component.
+        /// </summary>
+        public float Imaginary
+        { get; private set; }
+
+        /// <summary>
+        /// Gets the magnitude (or absolute value).
+        /// </summary>
+        public float Magnitude
+        { get; private set; }
+
+        /// <summary>
+        /// Gets the phase.
+        /// </summary>
+        public float Phase
+        { get; private set; }
+
+        #endregion
+
+        /// <summary>
+        /// Create a complex number with floats for real and imaginary components.
+        /// </summary>
+        /// <param name="real">Real component.</param>
+        /// <param name="img">Imaginary component.</param>
+        public Complex(float real = 0, float img = 0)
+        {
+            Real = real;
+            Imaginary = img;
+            Magnitude = (float)System.Math.Sqrt(real * real + img * img);
+            Phase = (float)System.Math.Atan2(img, real);
+        }
+
+        #region Operators
+
+        /// <summary>
+        /// Add a complex number to another.
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns></returns>
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+        }
+
+        /// <summary>
+        /// Subtract a complex number by another.
+        /// </summary>
+        /// <param name="c1">Number being subtracted from.</param>
+        /// <param name="c2">Number being subtracted.</param>
+        /// <returns></returns>
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            return new Complex(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
+        }
+
+        /// <summary>
+        /// Negate a complex number.
+        /// </summary>
+        /// <param name="c">Complex number to negate.</param>
+        /// <returns></returns>
+        public static Complex operator -(Complex c)
+        {
+            return new Complex(-c.Real, -c.Imaginary);
+        }
+
+        /// <summary>
+        /// Multiply two complex numbers.
+        /// </summary>
+        /// <param name="c1">First complex factor.</param>
+        /// <param name="c2">Second complex factor.</param>
+        /// <returns></returns>
+        public static Complex operator *(Complex c1, Complex c2)
+        {
+            return new Complex(c1.Real * c2.Real + -(c1.Imaginary * c2.Imaginary), c1.Real * c2.Imaginary + c1.Imaginary * c2.Real);
+        }
+
+        /// <summary>
+        /// Multiply a complex number by a regular float.
+        /// </summary>
+        /// <param name="c1">Complex factor.</param>
+        /// <param name="f1">Float factor.</param>
+        /// <returns></returns>
+        public static Complex operator *(Complex c1, float f1)
+        {
+            return new Complex(f1 * c1.Real, f1 * c1.Imaginary);
+        }
+
+        /// <summary>
+        /// Multiply a complex number by a regular float.
+        /// </summary>
+        /// <param name="f1">Float factor.</param>
+        /// <param name="c1">Complex factor.</param>
+        /// <returns></returns>
+        public static Complex operator *(float f1, Complex c1)
+        {
+            return new Complex(f1 * c1.Real, f1 * c1.Imaginary);
+        }
+
+        /// <summary>
+        /// Divide a complex number by another.
+        /// </summary>
+        /// <param name="dividend">Complex number to be divided.</param>
+        /// <param name="divisor">Complex number to divide by.</param>
+        /// <returns></returns>
+        public static Complex operator /(Complex dividend, Complex divisor)
+        {
+            // Algorithm is:
+            // ((ac + bd) / (c^2 + d^2)) + ((bc - ad) / (c^2 + d^2)i
+            // where dividend is a + bi, and divisor is b + ci 
+
+            float a = dividend.Real, b = dividend.Imaginary;
+            float c = divisor.Real, d = divisor.Imaginary;
+
+            return new Complex(((a * c + b * d) / (c * c + d * d)), ((b * c - a * d) / (c * c + d * d)));
+        }
+
+        /// <summary>
+        /// Devide a complex number by a regular float.
+        /// </summary>
+        /// <param name="dividend">Complex number to be divided.</param>
+        /// <param name="divisor">Float to divide by.</param>
+        /// <returns></returns>
+        public static Complex operator /(Complex dividend, float divisor)
+        {
+            return new Complex(dividend.Real / divisor, dividend.Imaginary / divisor);
+        }
+
+        /// <summary>
+        /// Check if two Complex numbers are equal.
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Complex c1, Complex c2)
+        {
+            return c1.Real.Equals(c2.Real) && c1.Imaginary.Equals(c2.Imaginary);
+        }
+
+        /// <summary>
+        /// Check if two Complex numbers are not equal.
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Complex c1, Complex c2)
+        {
+            return !(c1 == c2);
+        }
+
+        /// <summary>
+        /// Implicitly convert float to the complex number with said float as the real component.
+        /// </summary>
+        /// <param name="real"></param>
+        public static implicit operator Complex(float real)
+        {
+            return new Complex(real);
+        }
+
+        /// <summary>
+        /// Explicitly convert complex number to the string representation.
+        /// </summary>
+        /// <param name="c1"></param>
+        public static explicit operator string (Complex c1)
+        {
+            return c1.ToString();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Print out text representation of the complex number.
+        /// </summary>
+        /// <param name="realFormat">Formating for real part float</param>
+        /// <param name="imgFormat">Formating for imaginary part float</param>
+        /// <returns></returns>
+        public string ToString(string realFormat, string imgFormat)
+        {
+            return "(" + Real.ToString(realFormat) + " + " + Imaginary.ToString(imgFormat) + "i)";
+        }
+
+        #endregion
+
+        #region Overrides
+
+        /// <summary>
+        /// Gets the comjugate.
+        /// </summary>
+        /// <returns></returns>
+        public Complex Conjugate()
+        {
+            return new Complex(Real, -Imaginary);
+        }
+
+        /// <summary>
+        /// Check if this complex number is equal to an object passed
+        /// </summary>
+        /// <param name="obj">Any object which can be casted to Complex type.</param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            return this == (Complex)obj;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return (int)Real;
+        }
+
+        /// <summary>
+        /// Print out text representation of the complex number.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "(" + Real + " + " + Imaginary + "i)";
+        }
+
+        #endregion
+    }
+}
--- ACDC-Control/ACDC-Control/Program.cs
+++ ACDC-Control/ACDC-Control/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.SPOT;
+﻿using ACDC_Control.DSP.Types;
+using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 using Microsoft.SPOT.Net.NetworkInformation;
 using NetduinoGo;
@@ -11,6 +12,14 @@ namespace ACDC_Control
     public class Program
     {
         public static void Main()
+        {
+            ComplexNumTests();
+        }
+
+        /// <summary>
+        /// Core set of tests.
+        /// </summary>
+        private static void MainTests()
         {
             NetworkInterface wifi = Microsoft.SPOT.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()[0];
             // Lets keep the button for reseting for now... 
@@ -32,6 +41,44 @@ namespace ACDC_Control
 
             Debug.Print("Got IP: " + wifi.IPAddress);
             lightOutput.SetColor(0, 255, 0);
+        }
+        
+        /// <summary>
+        /// Test complex number type.
+        /// </summary>
+        private static void ComplexNumTests()
+        {
+            Complex c1 = 1, c2 = 0, c3 = Complex.Unit;
+            Complex c4 = new Complex(2, 2);
+            Complex c5 = new Complex(2, -4);
+            Complex c6 = new Complex(5, 5);
+            Complex c7 = new Complex(3, 9);
+
+            Debug.Print("Float 1 becomes: " + c1);
+            Debug.Print("Float 0 becomes: " + c2);
+            Debug.Print("Unit Complex is: " + c3);
+            Debug.Print("The conjugate of " + c4 + " is " + c4.Conjugate());
+            Debug.Print("Magnitude and Phase of " + c4 + " are " + c4.Magnitude + " and " + c4.Phase + " radians");
+            Debug.Print(c4 + " + " + c5 + " is " + (c4 + c5));
+            Debug.Print(c6 + " * 2 is " + (c6 * 2));
+            Debug.Print(c6 + " / 5 is " + (c6 / 5));
+            Debug.Print(c6 + " * " + c4 + " is " + (c6 * c4) + " with phase " + ((c6 * c4).Phase * (180 / System.Math.PI)) + " degrees");
+            Debug.Print(c7 + " + 3 is " + (c7 + 3));
+
+            /*
+            Print out confirms proper functioning:
+
+            Float 1 becomes: (1 + 0i)
+            Float 0 becomes: (0 + 0i)
+            Unit Complex is: (1 + 1i)
+            The conjugate of (2 + 2i) is (2 + -2i)
+            Magnitude and Phase of (2 + 2i) are 2.82842708 and 0.785398185 radians
+            (2 + 2i) + (2 + -4i) is (4 + -2i)
+            (5 + 5i) * 2 is (10 + 10i)
+            (5 + 5i) / 5 is (1 + 1i)
+            (5 + 5i) * (2 + 2i) is (0 + 20i) with phase 90.000002504478161 degrees
+            (3 + 9i) + 3 is (6 + 9i)
+            */
         }
     }
 }
